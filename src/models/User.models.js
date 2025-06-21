13a931b774536bc3bead2b1adcb55ed746f1f78c
ALI TAYYAB <01-134232-037@student.bahria.edu.pk>
2025-06-21 17:30:51 +0500

    Add initial files for database and middleware setup, and create User model schema

---
+++ src/models/User.models.js
@@ -0,0 +1,56 @@
+// /models/User.js
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+
+    verified: {
+      type: Boolean,
+      default: false,
+    },
+
+    otp: {
+      type: String,
+    },
+
+    otpExpiry: {
+      type: Date,
+    },
+
+    address: {
+      street: String,
+      city: String,
+      province: String,
+      postalCode: String,
+      country: {
+        type: String,
+        default: "Pakistan",
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
